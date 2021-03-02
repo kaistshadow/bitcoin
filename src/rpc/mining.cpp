f3d92ed1aa9cdec2996b37d2cd340992c9aa025b
Hyeojin Jhon <hyojin5658@gmail.com>
2021-03-02 13:23:44 +0900

    Fix transaction not included in new block
    1. change segwit height in chainparams.cpp

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -251,8 +251,8 @@ unsigned long long int _expected_mining_usec(unsigned int nBits) {
         default_random_source = &generator;
     }
 
-#define VIRTUAL_NODE_CNT 30
-#define EXP_BLK_NO  2513
+#define VIRTUAL_NODE_CNT 1
+#define EXP_BLK_NO  300
 #define EXP_SIMTIME_FOR_BLK 1000
 #define XFF_BITS 256
     arith_uint256 bnTarget;
